a40a60072a372080753b36b2bfa19fcaf9daf015
ThuyVan032004 <dovan032004@gmail.com>
2026-02-28 13:30:00 +0700

    update

--- src/main/java/vn/edu/hcmut/cse/adse/lab/config/DotenvConfig.java
+++ src/main/java/vn/edu/hcmut/cse/adse/lab/config/DotenvConfig.java
@@ -19,22 +19,14 @@ public class DotenvConfig implements ApplicationContextInitializer<ConfigurableA
 
         Map<String, Object> envVars = new HashMap<>();
 
-        String host = dotenv.get("POSTGRES_HOST", "localhost");
-        String port = dotenv.get("POSTGRES_PORT", "5432");
-        String db   = dotenv.get("POSTGRES_DB",   "student_management");
-        String user = dotenv.get("POSTGRES_USER",  "postgres");
-        String pass = dotenv.get("POSTGRES_PASSWORD", "");
+        String databaseUsername = dotenv.get("DB_USERNAME");
+        String databasePassword = dotenv.get("DB_PASSWORD");
+        
+        String databaseUrl = dotenv.get("DATABASE_URL");
 
-        envVars.put("POSTGRES_HOST", host);
-        envVars.put("POSTGRES_PORT", port);
-        envVars.put("POSTGRES_DB", db);
-        envVars.put("POSTGRES_USER", user);
-        envVars.put("POSTGRES_PASSWORD", pass);
-
-        envVars.put("DATABASE_URL",
-                "jdbc:postgresql://" + host + ":" + port + "/" + db);
-        envVars.put("DB_USERNAME", user);
-        envVars.put("DB_PASSWORD", pass);
+        envVars.put("DATABASE_URL", databaseUrl);
+        envVars.put("DB_USERNAME", databaseUsername);
+        envVars.put("DB_PASSWORD", databasePassword);
 
         applicationContext.getEnvironment().getPropertySources()
                 .addFirst(new MapPropertySource("dotenvProperties", envVars));
